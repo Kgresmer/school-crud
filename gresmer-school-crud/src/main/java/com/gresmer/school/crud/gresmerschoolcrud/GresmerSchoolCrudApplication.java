59dcf381ab36486a1d5a1b591b4a2c4bcd0206a0
kgresmer <kgresmer@gmail.com>
2019-08-02 08:43:11 -0500

    first commit

---
+++ gresmer-school-crud/src/main/java/com/gresmer/school/crud/gresmerschoolcrud/GresmerSchoolCrudApplication.java
@@ -0,0 +1,13 @@
+package com.gresmer.school.crud.gresmerschoolcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GresmerSchoolCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GresmerSchoolCrudApplication.class, args);
+	}
+
+}
